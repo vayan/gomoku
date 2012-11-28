f989701cfee2d902a749ada905bbcf6cdcc965e3
vayan <vaillant.y@gmail.com>
2012-11-28 20:44:24 +0100

    idk

--- js/gomoku.js
+++ js/gomoku.js
@@ -2,34 +2,39 @@ var ws;
 var turn = "black";
 
 if (ws != null) {
-     ws.close();
-     ws = null;
+	ws.close();
+	ws = null;
 }
 
 ws = new WebSocket("ws://localhost:1112/ws");
-  
+
 ws.onopen = function () {
 	console.log("open");
- };
-   
+};
+
 ws.onmessage = function (e) {
-   console.log("receive :"+e.data);
+	console.log("receive :"+e.data);
 
-   if (e.data != "error") {
+	if (e.data != "error") {
+		var data = e.data.split(',');
 
-   	var data = e.data.split(',');
- 	$('.pos'+data[0]+'y'+data[1]).addClass("bg"+data[2]);
- 	if (data[2] == "black") {
- 		turn = "white";
- 	} else {
- 		turn = "black";
- 	}
- 	$("#turn").text(turn+" Turn");
- 	}
+		if (data[2] == "remove"){
+			$('.pos'+data[0]+'y'+data[1]).removeClass("bgblack");
+			$('.pos'+data[0]+'y'+data[1]).removeClass("bgwhite");
+		} else {
+			$('.pos'+data[0]+'y'+data[1]).addClass("bg"+data[2]);
+			if (data[2] == "black") {
+				turn = "white";
+			} else {
+				turn = "black";
+			}
+			$("#turn").text(turn+" Turn");
+		}
+	}
 };
 
 ws.onclose = function (e) {
-   console.log("closed");
+	console.log("closed");
 };
 
 $("#turn").text(turn+" Turn");
